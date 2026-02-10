42f955c7c39891420ecd548618e568467e629750
Dimitris Vogiatzis <dvogiatzis@confluent.io>
2026-02-10 17:32:56 +0100

    Add example for CSFLE in CP with Azure KMS

---
+++ confluent_platform/azure/python/avro_producer.py
@@ -0,0 +1,101 @@
+import os
+from datetime import datetime, timezone, date
+
+from confluent_kafka import Producer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.schema_registry.rules.encryption.azurekms.azure_driver import AzureKmsDriver
+from confluent_kafka.schema_registry.rules.encryption.encrypt_executor import FieldEncryptionExecutor
+from confluent_kafka.serialization import MessageField, SerializationContext, StringSerializer
+from dateutil.relativedelta import relativedelta
+
+import config
+
+
+class PersonalData(object):
+
+    def __init__(self, id, name, birthday, timestamp):
+        self.id = id
+        self.name = name
+        self.birthday = birthday
+        self.timestamp = timestamp
+
+
+def personal_data_to_dict(data, ctx):
+    return dict(
+        id=data.id,
+        name=data.name,
+        birthday=data.birthday,
+        timestamp=data.timestamp
+    )
+
+
+def delivery_report(err, msg):
+    if err is not None:
+        print("Delivery failed for record {}: {}".format(msg.key(), err))
+        return
+    print(
+        'PersonalData record {} successfully produced to {} [{}] at offset {}'.format(
+            msg.key(), msg.topic(), msg.partition(), msg.offset()
+        )
+    )
+
+
+def main():
+    AzureKmsDriver.register()
+    FieldEncryptionExecutor.register()
+
+    schema = "personal_data.avsc"
+    path = os.path.realpath(os.path.dirname(__file__))
+    with open(f"{path}/avro/{schema}") as f:
+        schema_str = f.read()
+
+    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+
+    ser_conf = {'auto.register.schemas': False, 'use.latest.version': True}
+    avro_serializer = AvroSerializer(schema_registry_client, schema_str, personal_data_to_dict, conf=ser_conf)
+
+    string_serializer = StringSerializer('utf_8')
+
+    producer = Producer(producer_conf)
+
+    print("Producing user records to topic {}. ^C to exit.".format(topic))
+    i = 0
+    while i < 20:
+        # Serve on_delivery callbacks from previous calls to produce()
+        i = i + 1
+        producer.poll(0.0)
+        try:
+            personal_data = PersonalData(
+                id=str(i),
+                name='Anna',
+                birthday=(date.today() - relativedelta(years=i)).strftime('%Y-%m-%d'),
+                timestamp=datetime.now(timezone.utc).isoformat()
+            )
+            producer.produce(
+                topic=topic,
+                key=string_serializer(str(i)),
+                value=avro_serializer(personal_data, SerializationContext(topic, MessageField.VALUE)),
+                on_delivery=delivery_report,
+            )
+        except KeyboardInterrupt:
+            break
+        except ValueError:
+            print("Invalid input, discarding record...")
+            continue
+
+    print("\nFlushing records...")
+    producer.flush()
+
+
+# Load configuration from environment variables
+config.validate_config()
+config.setup_azure_credentials()
+
+topic = config.get_topic()
+schema_registry_conf = config.get_schema_registry_config()
+producer_conf = config.get_producer_config()
+kek_name, kms_type, kms_key_id = config.get_kms_config()
+
+if __name__ == '__main__':
+    main()
